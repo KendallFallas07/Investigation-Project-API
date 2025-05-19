2c8be2eae310538a2172e79c62ffd6063f3f85ac
KendallFallas07 <kendallfallas2018@gmail.com>
2025-05-19 09:40:14 -0600

    Add initial project setup with configuration files and basic API structure

---
+++ Investigation-Project-API/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.Extensions.FileProviders;
+
+DotNetEnv.Env.Load();
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+app.UseStaticFiles(); 
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "uploadedImages")),
+    RequestPath = "/UploadedImages" 
+});
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUi(options =>
+   {
+       options.DocumentPath = "/openapi/v1.json";
+   });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
