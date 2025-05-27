6995eaf808dc4d220bfa49f08afc33f61f189c16
Kendall Fallas Mena <117144574+KendallFallas07@users.noreply.github.com>
2025-05-26 22:14:01 -0600

    Dev review crud (#2)
    * Implement Review CRUD operations in ReviewData and ReviewController
    
    * Add stored procedure and controller method to retrieve reviews by game ID
    
    ---------
    
    Co-authored-by: Ceasar Calvo <ceasar.calvo.munoz@una.ac.cr>

---
+++ Investigation-Project-API/Controller/ReviewController.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using System.Collections.Generic;
+
+namespace Controller;
+
+[ApiController]
+[Route("api/reviews")]
+public class ReviewController : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<Review> Get()
+    {
+        return new ReviewData().GetAllReviews();
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+        var review = new ReviewData().GetReviewById(id);
+        if (review == null)
+        {
+            return NotFound();
+        }
+        return Ok(review);
+    }
+
+    [HttpGet("game/{id}")]
+    public IEnumerable<Review> GetGameReviews(int id)
+    {
+        return new ReviewData().GetGameReviewsById(id);
+    }
+
+    [HttpPost]
+    public IActionResult Post(int gameId,string reviewerName,string comment,int rating)
+    {
+        var insertedReview = new ReviewData().InsertReview(gameId, reviewerName, comment, rating);
+        return Ok(insertedReview);
+    }
+
+    [HttpPut]
+    public IActionResult Put( int id,int gameId,string reviewerName,string comment,int rating)
+    {
+        var review = new ReviewData().GetReviewById(id);
+        if (review == null)
+        {
+            return NotFound();
+        }
+
+        var updatedReview = new ReviewData().UpdateReview(id, gameId, reviewerName, comment, rating);
+        return Ok(updatedReview);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var review = new ReviewData().GetReviewById(id);
+        if (review == null)
+        {
+            return NotFound();
+        }
+
+        var isDeleted = new ReviewData().DeleteReview(id);
+
+        if (isDeleted)
+        {
+            return BadRequest();
+        }
+
+        return Ok(new { message = "Review deleted successfully." });
+    }
+}
--- Investigation-Project-API/Data/ReviewData.cs
+++ Investigation-Project-API/Data/ReviewData.cs
@@ -6,29 +6,182 @@ class ReviewData : ConnectionSQL
 {
     public SqlConnection conn = GetConnect();
 
-    public Review? InsertReview()
+    public Review? InsertReview(int gameId, string reviewerName, string comment, int rating)
     {
+        var sqlCommand = new SqlCommand("CREATE_REVIEW", conn)
+        {
+            CommandType = CommandType.StoredProcedure
+        };
+
+        sqlCommand.Parameters.AddWithValue("@GAME_ID", gameId);
+        sqlCommand.Parameters.AddWithValue("@REVIEWER_NAME", reviewerName);
+        sqlCommand.Parameters.AddWithValue("@COMMENT", comment);
+        sqlCommand.Parameters.AddWithValue("@RATING", rating);
+
+        conn.Open();
+        SqlDataReader reader = sqlCommand.ExecuteReader();
+
+        if (reader.Read())
+        {
+            var review = new Review
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("ID")),
+                GameId = reader.GetInt32(reader.GetOrdinal("GAME_ID")),
+                ReviewerName = reader.GetString(reader.GetOrdinal("REVIEWER_NAME")),
+                Comment = reader.GetString(reader.GetOrdinal("COMMENT")),
+                Rating = reader.GetInt32(reader.GetOrdinal("RATING")),
+                ReviewDate = reader.GetDateTime(reader.GetOrdinal("REVIEW_DATE"))
+            };
+
+            conn.Close();
+            return review;
+        }
+
+        conn.Close();
         return null;
     }
 
-    public List<Review> getAllReviews()
+    public List<Review> GetAllReviews()
     {
+        List<Review> reviews = new();
+
+        var sqlCommand = new SqlCommand("SELECT_REVIEW", conn)
+        {
+            CommandType = CommandType.StoredProcedure
+        };
+
+        conn.Open();
+        SqlDataReader reader = sqlCommand.ExecuteReader();
+
+        while (reader.Read())
+        {
+            reviews.Add(new Review
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("ID")),
+                GameId = reader.GetInt32(reader.GetOrdinal("GAME_ID")),
+                ReviewerName = reader.GetString(reader.GetOrdinal("REVIEWER_NAME")),
+                Comment = reader.GetString(reader.GetOrdinal("COMMENT")),
+                Rating = reader.GetInt32(reader.GetOrdinal("RATING")),
+                ReviewDate = reader.GetDateTime(reader.GetOrdinal("REVIEW_DATE"))
+            });
+        }
+
+        conn.Close();
+        return reviews;
+    }
+
+    public Review? GetReviewById(int id)
+    {
+        var sqlCommand = new SqlCommand("SELECT_REVIEW_BY_ID", conn)
+        {
+            CommandType = CommandType.StoredProcedure
+        };
+
+        sqlCommand.Parameters.AddWithValue("@ID", id);
+
+        conn.Open();
+        SqlDataReader reader = sqlCommand.ExecuteReader();
+
+        if (reader.Read())
+        {
+            var review = new Review
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("ID")),
+                GameId = reader.GetInt32(reader.GetOrdinal("GAME_ID")),
+                ReviewerName = reader.GetString(reader.GetOrdinal("REVIEWER_NAME")),
+                Comment = reader.GetString(reader.GetOrdinal("COMMENT")),
+                Rating = reader.GetInt32(reader.GetOrdinal("RATING")),
+                ReviewDate = reader.GetDateTime(reader.GetOrdinal("REVIEW_DATE"))
+            };
+
+            conn.Close();
+            return review;
+        }
+
+        conn.Close();
         return null;
     }
 
-    public Review? updateReview()
+    public List<Review> GetGameReviewsById(int id)
     {
+
+        List<Review> reviews = new();
+
+        var sqlCommand = new SqlCommand("GET_REVIEWS_FROM_GAME", conn)
+        {
+            CommandType = CommandType.StoredProcedure
+        };
+
+        sqlCommand.Parameters.AddWithValue("@ID", id);
+
+        conn.Open();
+        SqlDataReader reader = sqlCommand.ExecuteReader();
+
+        while (reader.Read())
+        {
+            reviews.Add(new Review
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("ID")),
+                GameId = reader.GetInt32(reader.GetOrdinal("GAME_ID")),
+                ReviewerName = reader.GetString(reader.GetOrdinal("REVIEWER_NAME")),
+                Comment = reader.GetString(reader.GetOrdinal("COMMENT")),
+                Rating = reader.GetInt32(reader.GetOrdinal("RATING")),
+                ReviewDate = reader.GetDateTime(reader.GetOrdinal("REVIEW_DATE"))
+            });
+        }
+
+        conn.Close();
+        return reviews;
+    }
+
+    public Review? UpdateReview(int id, int gameId, string reviewerName, string comment, int rating)
+    {
+        var sqlCommand = new SqlCommand("UPDATE_REVIEW", conn)
+        {
+            CommandType = CommandType.StoredProcedure
+        };
+
+        sqlCommand.Parameters.AddWithValue("@ID", id);
+        sqlCommand.Parameters.AddWithValue("@GAME_ID", gameId);
+        sqlCommand.Parameters.AddWithValue("@REVIEWER_NAME", reviewerName);
+        sqlCommand.Parameters.AddWithValue("@COMMENT", comment);
+        sqlCommand.Parameters.AddWithValue("@RATING", rating);
+
+        conn.Open();
+        SqlDataReader reader = sqlCommand.ExecuteReader();
+
+        if (reader.Read())
+        {
+            var review = new Review
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("ID")),
+                GameId = reader.GetInt32(reader.GetOrdinal("GAME_ID")),
+                ReviewerName = reader.GetString(reader.GetOrdinal("REVIEWER_NAME")),
+                Comment = reader.GetString(reader.GetOrdinal("COMMENT")),
+                Rating = reader.GetInt32(reader.GetOrdinal("RATING")),
+                ReviewDate = reader.GetDateTime(reader.GetOrdinal("REVIEW_DATE"))
+            };
+
+            conn.Close();
+            return review;
+        }
+
+        conn.Close();
         return null;
     }
 
-    public bool deleteReview()
+    public bool DeleteReview(int id)
     {
-        return false;
-    }
+        var sqlCommand = new SqlCommand("DELETE_REVIEW", conn)
+        {
+            CommandType = CommandType.StoredProcedure
+        };
 
-    public Review? GetReviewById()
-    {
-        return null;
-    }
+        sqlCommand.Parameters.AddWithValue("@ID", id);
 
+        conn.Open();
+        int affectedRows = sqlCommand.ExecuteNonQuery();
+        conn.Close();
+        return affectedRows > 0;
+    }
 }
